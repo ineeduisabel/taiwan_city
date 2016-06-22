52f5b9a1e5d01b95f537baa9ccc23d8876894386
Tim <timtsai@racbit.com>
2016-06-22 15:28:03 +0800

    i18n

--- lib/taiwan_city.rb
+++ lib/taiwan_city.rb
@@ -58,7 +58,7 @@ module TaiwanCity
         # }
         @list = {}
         #@see: http://github.com/RobinQu/LocationSelect-Plugin/raw/master/areas_1.0.json
-        json = JSON.parse(File.read("#{Engine.root}/db/areas.json"))
+        json = JSON.parse(File.read("#{Engine.root}/db/areas-#{I18n.locale}.json"))
         districts = json.values.flatten
         districts.each do |district|
           id = district['id']
