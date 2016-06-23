39edf1b72bc6bccc0328fe7f26acfe4584040f77
Tim <timtsai@racbit.com>
2016-06-23 11:49:00 +0800

    i18n = session

--- app/controllers/taiwan_city/data_controller.rb
+++ app/controllers/taiwan_city/data_controller.rb
@@ -3,8 +3,8 @@ require_dependency "taiwan_city/application_controller"
 module TaiwanCity
   class DataController < ApplicationController
     def show
+      I18n.locale=session[:locale]
       data = TaiwanCity.list(params[:id])
-      binding.pry
       render json: data, layout: nil
     end
 
