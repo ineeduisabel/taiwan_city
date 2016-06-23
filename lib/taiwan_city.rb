392cb67789a7ca8a8ca0a2e5425d9b9b1da8b0df
Tim <timtsai@racbit.com>
2016-06-23 10:55:28 +0800

    change locale to session[:locale]

--- lib/taiwan_city.rb
+++ lib/taiwan_city.rb
@@ -57,7 +57,7 @@ module TaiwanCity
         # }
         @list = {}
         #@see: http://github.com/RobinQu/LocationSelect-Plugin/raw/master/areas_1.0.json
-        json = JSON.parse(File.read("#{Engine.root}/db/areas-#{I18n.locale}.json"))
+        json = JSON.parse(File.read("#{Engine.root}/db/areas-#{session[:locale]}.json"))
         districts = json.values.flatten
         districts.each do |district|
           id = district['id']
